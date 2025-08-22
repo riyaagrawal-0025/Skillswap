16b5c31fa8eff13ae32731d0d60f9a3074452830
riyaagrawal-0025 <riyaagrawal0205@gmail.com>
2025-08-22 23:59:14 +0530

    signup page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home'
 import About from './pages/About'
 import ContactUs from './pages/ContactUs'
 import Login from './pages/Login'
+import SignUp from './pages/SignUp'
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/about" element={<About  />} />
         <Route path="/contact" element={<ContactUs />} />
         <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<SignUp/>} />
        
       </Routes>
     </Router>
